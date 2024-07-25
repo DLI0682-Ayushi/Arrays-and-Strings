4bb3a864b42ed8eadbe2a90e3d0e2108a1f931db
ayushipundir13 <ayushi.pundir@godigit.com>
2024-07-25 16:26:25 +0530

    Checking string is anagram or not

---
+++ Anagram.java
@@ -0,0 +1,32 @@
+package stringproblems;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Anagram {
+    public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                System.out.print("Enter the first string: ");
+                String str1 = input.nextLine();
+                System.out.print("Enter the second string: ");
+                String str2 = input.nextLine();
+                if (str1.length() == str2.length()) {
+                    str1 = str1.toLowerCase();
+                    str2 = str2.toLowerCase();
+                    char[] charArray1 = str1.toCharArray();
+                    char[] charArray2 = str2.toCharArray();
+
+                    Arrays.sort(charArray1);
+                    Arrays.sort(charArray2);
+                    boolean result = Arrays.equals(charArray1, charArray2);
+
+                    if (result) {
+                        System.out.println(str1 + " and " + str2 + " are anagrams.");
+                    } else {
+                        System.out.println(str1 + " and " + str2 + " are not anagrams.");
+                    }
+                } else {
+                    System.out.println(str1 + " and " + str2 + " are not anagrams.");
+                }
+
+                input.close();
+            }
+        }
